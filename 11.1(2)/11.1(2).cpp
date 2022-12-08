779d8a1d87a93d74fadf423da9bb2f1b7ff86abd
revelations? <flfkflfkkf@gmail.com>
2022-12-08 17:49:50 +0200

    Добавьте файлы проекта.

---
+++ 11.1(2)/11.1(2).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char city;
+	cout << "Enter the first letter of the city name: ";
+	cin >> city;
+
+	if (city == 'C')
+		cout << "\nChernihiv\nPopulation: 0,282 million\nArea: 79 sq km\n";
+	else if (city == 'D')
+		cout << "\nDnipro\nPopulation: 0,968 million\nArea: 409,7 sq km\n";
+	else if (city == 'K')
+		cout << "\nKyiv\nPopulation: 2,966 million\nArea: 839 sq km\n";
+	else if (city == 'L')
+		cout << "\nLviv\nPopulation: 0,717 million\nArea: 149 sq km\n";
+	else if (city == 'M')
+		cout << "\nMariupol\nPopulation: 0,446 million\nArea: 166 sq km\n";
+	else if (city == 'O')
+		cout << "\nOdesa\nPopulation: 1,010 million\nArea: 162,4 sq km\n";
+	else if (city == 'Z')
+		cout << "\nZhytomyr\nPopulation: 0,265 million\nArea: 65 sq km\n";
+	else
+		cout << "\nThere is no such city or it is not entered in the database.\n";
+}
